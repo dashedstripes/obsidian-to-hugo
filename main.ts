481f5430470384120e8aac080839bf54483bd2a8
Adam Gray <adamgrayscale@gmail.com>
2022-12-17 23:23:07 -0600

    base plugin built

--- main.ts
+++ main.ts
@@ -1,81 +1,33 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as fs from 'fs';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ExportToHugoSettings {
+	hugoDir: string;
+	exportFlag: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ExportToHugoSettings = {
+	hugoDir: '/',
+	exportFlag: '~export'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function slugify(text: string) {
+  return text.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+}
+
+export default class ExportToHugo extends Plugin {
+	settings: ExportToHugoSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this gets called on save
+		this.registerEvent(this.app.vault.on('modify', async (e) => {
+			this.onModify(e.name);
+    }));
 	}
 
 	onunload() {
@@ -89,28 +41,49 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async onModify(currentNoteName: string) {
+		// 1. get the contents of the current note
+		const currentNote = this.app.workspace.getActiveFile();
+		if(!currentNote) return; // Nothing Open
+
+		let text = await this.app.vault.read(currentNote);
+
+		// 2. create a copy of this node in the hugo directory
+
+		if(text.substring(0, this.settings.exportFlag.length) === this.settings.exportFlag) {
+			
+			let content = text.split('\n');
+			content = content.slice(2);
+
+			let noteTitle = currentNoteName.split('.')[0];
+
+			let newText = content.join('\n');
+
+			newText = 
+`---
+title: ${noteTitle}
+---
+
+` + newText;
+
+			try {
+				fs.writeFileSync(`${this.settings.hugoDir}/${slugify(noteTitle)}.md`, newText);
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	}
+
+	shouldSaveFile(): boolean {
+		return false;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: ExportToHugo;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ExportToHugo) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +93,27 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Hugo Export.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Hugo Directory')
+			.setDesc('Where would you like generated files to go?')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('/Users/Library/etc')
+				.setValue(this.plugin.settings.hugoDir)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.hugoDir = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Export Flag')
+			.setDesc('What keyword would you like to use as your export flag?')
+			.addText(text => text
+				.setPlaceholder('~export')
+				.setValue(this.plugin.settings.exportFlag)
+				.onChange(async (value) => {
+					this.plugin.settings.exportFlag = value;
 					await this.plugin.saveSettings();
 				}));
 	}
